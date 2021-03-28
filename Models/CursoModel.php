52f861eb9a570cd35a74e701b32bd6f9cb61b822
marco <marco@192.168.15.4>
2021-03-28 17:31:12 -0300

    Recmovido comentario

--- Models/CursoModel.php
+++ Models/CursoModel.php
@@ -13,30 +13,4 @@ class CursoModel extends Model {
         return $DADOS;
     }
 
-    /*
-      public function incluir($DADOS) {
-      $prepare = $this->pdo->prepare('
-      INSERT INTO CURSO ( NOME ) VALUES (:NOME)
-      ');
-
-      return $prepare->execute($DADOS);
-      }
-
-      public function excluir($DADOS) {
-      $prepare = $this->pdo->prepare("
-      DELETE FROM CURSO WHERE $this->ID_CHAVE = :$this->ID_CHAVE
-      ");
-
-      return $prepare->execute($DADOS);
-      }
-
-      public function alterar($DADOS) {
-      $prepare = $this->pdo->prepare("
-      UPDATE CURSO SET NOME = :NOME
-      WHERE $this->ID_CHAVE = :$this->ID_CHAVE
-      ");
-
-      return $prepare->execute($DADOS);
-      }
-     */
 }
